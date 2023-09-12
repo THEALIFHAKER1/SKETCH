40b565c7d16e5fc3aeaa05a528f9fd4b4237add8
THEALIFHAKER1 <alifhaker1@gmail.com>
2023-09-12 21:34:28 +0800

    d

--- Functions/users.js
+++ Functions/users.js
@@ -1,27 +1,38 @@
 function SaveUser(username, password) {
-    let users = GetData('users') || []; 
+  let users = GetData("users") || [];
 
-    users.push({
-        username: username,
-        password: password
-    });
+  users.push({
+    username: username,
+    password: password,
+  });
 
-    SaveData('users', users);
+  SaveData("users", users);
 }
 
 function GetUser(username, password) {
-    let users = GetData('users') || []; 
+  let users = GetData("users") || [];
 
-    const user = users.find(user => user.username == username && user.password == password);
-    
-    return user 
-    ? (SaveData('LoggedUser', user), true) : (SaveData('LoggedUser', null), false);
+  const user = users.find(
+    (user) => user.username == username && user.password == password
+  );
+
+  return user
+    ? (SaveData("LoggedUser", user), true)
+    : (SaveData("LoggedUser", null), false);
 }
 
+// login as guest
+function Guest() {
+  user = {
+    username: "guest",
+    password: null,
+  };
+  SaveData("LoggedUser", user);
+}
 function GetLoggedUser() {
-    return GetData('LoggedUser');
+  return GetData("LoggedUser");
 }
 
 function LogoutUser() {
-    SaveData('LoggedUser', null);
+  SaveData("LoggedUser", null);
 }
