461c842a04bb0a8caf32b8c2f413cc19dae33a28
MUHAMMAD ALIF DANIEL <alifhaker1@gmail.com>
2023-09-14 19:44:50 +0800

    Add files via upload

--- Functions/canvas.js
+++ Functions/canvas.js
@@ -1,4 +1,4 @@
-function SaveCanvas(title, dataURL, username, Public) {
+function SaveCanvas(title, dataURL, username, Public, date) {
   let canvas = GetData("canvas") || [];
 
   if (canvas) {
@@ -7,12 +7,35 @@ function SaveCanvas(title, dataURL, username, Public) {
       canvas: dataURL,
       username: username,
       Public: Public,
+      Date: date,
     });
     SaveData("canvas", canvas);
     alert("Canvas saved");
   } else {
     alert("Canvas not saved");
   }
+}
+
+//  save  edited canvas by replace canvas that has same date
+
+function SaveEditedCanvas(title, dataURL, username, Public, date) {
+  let canvas = GetData("canvas") || [];
+  let newCanvas = canvas.filter((item) => item.Date !== date);
+  newCanvas.push({
+    title: title,
+    canvas: dataURL,
+    username: username,
+    Public: Public,
+    Date: date,
+  });
+  SaveData("canvas", newCanvas);
+  alert("Canvas saved");
+}
+
+function DeleteCanvas(date) {
+  let canvas = GetData("canvas") || [];
+  let newCanvas = canvas.filter((item) => item.Date !== date);
+  SaveData("canvas", newCanvas);
 }
 
 // get public canvas
